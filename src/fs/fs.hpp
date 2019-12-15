966c2e1f3b06b5991f2bba6f7cbb2fdcb51365ff
xhighway999 <xhighway999@protonmail.com>
2019-12-15 14:24:59 +0100

    Fix formatting

--- src/fs/fs.hpp
+++ src/fs/fs.hpp
@@ -5,26 +5,27 @@
 #include <filesystem>
 #include <string_view>
 #include <vector>
+
 #include "physfs.h"
 namespace xhfr {
 namespace fs {
-  enum FileType { Regular, Directory, Symlink, Other };
-  struct FileInfo {
-    std::string name, path;
-    long long size;
-    FileType type;
-    bool readonly;
-    bool operator==(const FileInfo& fileInfo) const {
-      return path == fileInfo.path;
-    }
-  };
-
-  bool init(const char* path);
-  void shutdown();
-  bool createDirectory(std::string_view path);
-  std::vector<FileInfo> listFiles(std::string_view path);
-  bool deleteFile(std::string_view path);
+enum FileType { Regular, Directory, Symlink, Other };
+struct FileInfo {
+  std::string name, path;
+  long long size;
+  FileType type;
+  bool readonly;
+  bool operator==(const FileInfo& fileInfo) const {
+    return path == fileInfo.path;
+  }
 };
+
+bool init(const char* path);
+void shutdown();
+bool createDirectory(std::string_view path);
+std::vector<FileInfo> listFiles(std::string_view path);
+bool deleteFile(std::string_view path);
+};  // namespace fs
 }  // namespace xhfr
 
 #endif  // FS_HPP
