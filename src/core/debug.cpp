2aaf4f3b97e3ab8336071057b77aea7bc59a3eb2
Quentin Kammann <xhighway999@protonmail.com>
2020-08-28 13:15:45 +0200

    Fix name

--- src/core/debug.cpp
+++ src/core/debug.cpp
@@ -8,7 +8,7 @@ void debug::initLogging() {
   auto sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
   sink->set_level(spdlog::level::trace);
 
-  logger = new spdlog::logger("EDGE_Logger", {sink});
+  logger = new spdlog::logger("XHFR_Logger", {sink});
   logger->set_level(spdlog::level::trace);
   spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%l] [%s:%#] %v");
   XHFR_INFO("Logger init done");
