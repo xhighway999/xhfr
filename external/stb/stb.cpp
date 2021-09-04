14d684b84a36b051d3eca3ea2b182993d9b1f7e2
Quentin Kammann <xhighway999@protonmail.com>
2021-09-04 14:04:24 +0200

    update stb impl

--- external/stb/stb.cpp
+++ external/stb/stb.cpp
@@ -2,7 +2,6 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_LEAKCHECK_IMPLEMENTATION
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb/stb.h"
 #include "stb/stb_c_lexer.h"
 #include "stb/stb_connected_components.h"
 #include "stb/stb_divide.h"
@@ -15,14 +14,12 @@
 #include "stb/stb_image_write.h"
 #include "stb/stb_include.h"
 #include "stb/stb_leakcheck.h"
-#include "stb/stb_perlin.h"
 #include "stb/stb_rect_pack.h"
 #include "stb/stb_sprintf.h"
 #include "stb/stb_textedit.h"
 #include "stb/stb_tilemap_editor.h"
 #include "stb/stb_truetype.h"
 #include "stb/stb_voxel_render.h"
-#include "stb/stretchy_buffer.h"
 
 #undef STB_IMAGE_WRITE_IMPLEMENTATION
 
