d83cbc558cb9416e2d966402d76e9bfb3129e52e
Quentin Kammann <xhighway999@protonmail.com>
2021-10-13 16:23:05 +0200

    Add ImageButtonWithText and added return to Links

--- src/gui/Widgets.cpp
+++ src/gui/Widgets.cpp
@@ -86,7 +86,7 @@ bool ImGui::InputTextMultiline(const char* label,
                             size, flags, InputTextCallback, &cb_user_data);
 }
 
-void ImGui::Link(const char* linkText, const char* link, ImColor color) {
+bool ImGui::Link(const char* linkText, const char* link, ImColor color) {
   auto pos = ImGui::GetCursorScreenPos();
   // https://github.com/xhighway999/luapad
 
@@ -102,5 +102,96 @@ void ImGui::Link(const char* linkText, const char* link, ImColor color) {
   draw_list->AddLine(pos, ImVec2(pos.x + size.x, pos.y), color, 1);
   if (ImGui::IsItemClicked()) {
     xhfr::openURL(link);
+    return true;
   }
+  return false;
+}
+
+bool ImageButtonWithText(ImTextureID texId,
+                         std::string label,
+                         const ImVec2& imageSize,
+                         const ImVec2& uv0,
+                         const ImVec2& uv1,
+                         int frame_padding,
+                         const ImVec4& bg_col,
+                         const ImVec4& tint_col) {
+  return ImageButtonWithText(texId, label.c_str(), imageSize, uv0, uv1,
+                             frame_padding, bg_col, tint_col);
+}
+
+bool ImGui::ImageButtonWithText(ImTextureID texId,
+                                const char* label,
+                                const ImVec2& imageSize,
+                                const ImVec2& uv0,
+                                const ImVec2& uv1,
+                                int frame_padding,
+                                const ImVec4& bg_col,
+                                const ImVec4& tint_col) {
+  ImGuiWindow* window = GetCurrentWindow();
+  if (window->SkipItems)
+    return false;
+
+  ImVec2 size = imageSize;
+  if (size.x <= 0 && size.y <= 0) {
+    size.x = size.y = ImGui::GetTextLineHeightWithSpacing();
+  } else {
+    if (size.x <= 0)
+      size.x = size.y;
+    else if (size.y <= 0)
+      size.y = size.x;
+    size *= window->FontWindowScale * ImGui::GetIO().FontGlobalScale;
+  }
+
+  ImGuiContext& g = *GImGui;
+  const ImGuiStyle& style = g.Style;
+
+  const ImGuiID id = window->GetID(label);
+  const ImVec2 textSize = ImGui::CalcTextSize(label, NULL, true);
+  const bool hasText = textSize.x > 0;
+
+  const float innerSpacing =
+      hasText ? ((frame_padding >= 0) ? (float)frame_padding
+                                      : (style.ItemInnerSpacing.x))
+              : 0.f;
+  const ImVec2 padding =
+      (frame_padding >= 0) ? ImVec2((float)frame_padding, (float)frame_padding)
+                           : style.FramePadding;
+  const ImVec2 totalSizeWithoutPadding(
+      size.x + innerSpacing + textSize.x,
+      size.y > textSize.y ? size.y : textSize.y);
+  const ImRect bb(window->DC.CursorPos,
+                  window->DC.CursorPos + totalSizeWithoutPadding + padding * 2);
+  ImVec2 start(0, 0);
+  start = window->DC.CursorPos + padding;
+  if (size.y < textSize.y)
+    start.y += (textSize.y - size.y) * .5f;
+  const ImRect image_bb(start, start + size);
+  start = window->DC.CursorPos + padding;
+  start.x += size.x + innerSpacing;
+  if (size.y > textSize.y)
+    start.y += (size.y - textSize.y) * .5f;
+  ItemSize(bb);
+  if (!ItemAdd(bb, id))
+    return false;
+
+  bool hovered = false, held = false;
+  bool pressed = ButtonBehavior(bb, id, &hovered, &held);
+
+  // Render
+  const ImU32 col = GetColorU32((hovered && held) ? ImGuiCol_ButtonActive
+                                : hovered         ? ImGuiCol_ButtonHovered
+                                                  : ImGuiCol_Button);
+  RenderFrame(
+      bb.Min, bb.Max, col, true,
+      ImClamp((float)ImMin(padding.x, padding.y), 0.0f, style.FrameRounding));
+  if (bg_col.w > 0.0f)
+    window->DrawList->AddRectFilled(image_bb.Min, image_bb.Max,
+                                    GetColorU32(bg_col));
+
+  window->DrawList->AddImage(texId, image_bb.Min, image_bb.Max, uv0, uv1,
+                             GetColorU32(tint_col));
+
+  if (textSize.x > 0)
+    ImGui::RenderText(start, label);
+  return pressed;
 }
--- src/gui/Widgets.hpp
+++ src/gui/Widgets.hpp
@@ -24,7 +24,7 @@ struct InputTextCallback_UserData {
   void* ChainCallbackUserData;
 };
 
-void Link(const char* linkText,
+bool Link(const char* linkText,
           const char* link = nullptr,
           ImColor color = ImColor(0, 0, 255));
 
@@ -39,5 +39,23 @@ bool InputTextMultiline(const char* label,
                         ImGuiInputTextFlags flags = 0,
                         ImGuiInputTextCallback callback = nullptr,
                         void* user_data = nullptr);
+
+bool ImageButtonWithText(ImTextureID texId,
+                         std::string label,
+                         const ImVec2& imageSize = ImVec2(0, 0),
+                         const ImVec2& uv0 = ImVec2(0, 0),
+                         const ImVec2& uv1 = ImVec2(1, 1),
+                         int frame_padding = -1,
+                         const ImVec4& bg_col = ImVec4(0, 0, 0, 0),
+                         const ImVec4& tint_col = ImVec4(1, 1, 1, 1));
+
+bool ImageButtonWithText(ImTextureID texId,
+                         const char* label,
+                         const ImVec2& imageSize = ImVec2(0, 0),
+                         const ImVec2& uv0 = ImVec2(0, 0),
+                         const ImVec2& uv1 = ImVec2(1, 1),
+                         int frame_padding = -1,
+                         const ImVec4& bg_col = ImVec4(0, 0, 0, 0),
+                         const ImVec4& tint_col = ImVec4(1, 1, 1, 1));
 
 }  // namespace ImGui
